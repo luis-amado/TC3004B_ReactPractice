7fdcc8cce2bda68debab7adc9c259f1f70d44a6e
Luis <lamadoalvarez@gmail.com>
2025-05-15 12:01:50 -0600

    naming issue

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import ConditionalRoute from './components/AuthedRoute';
 
-const REACT_APP_API_URL = process.env.API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 const App = () => {
   const [items, setItems] = useState([]);
@@ -17,7 +17,7 @@ const App = () => {
   const [token, setToken] = useState('');
 
   const getItems = async () => {
-    const response = await fetch(`${REACT_APP_API_URL}/items`, {
+    const response = await fetch(`${API_URL}/items`, {
       headers: { Authorization: token },
     });
     const data = await response.json();
@@ -31,7 +31,7 @@ const App = () => {
   }, [isLogin]);
 
   const addItem = async (item) => {
-    const response = await fetch(`${REACT_APP_API_URL}/items`, {
+    const response = await fetch(`${API_URL}/items`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json', Authorization: token },
       body: JSON.stringify(item),
@@ -41,7 +41,7 @@ const App = () => {
   };
 
   const delItem = async (id) => {
-    await fetch(`${REACT_APP_API_URL}/items/${id}`, {
+    await fetch(`${API_URL}/items/${id}`, {
       method: 'DELETE',
       headers: { Authorization: token },
     });
@@ -49,7 +49,7 @@ const App = () => {
   };
 
   const login = async (user) => {
-    const response = await fetch(`${REACT_APP_API_URL}/login/`, {
+    const response = await fetch(`${API_URL}/login/`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(user),
